6e90094d184a9d8c85ef7c0b11653ac36d055065
lombareload@gmail.com <lombareload@hotmail.com>
2014-11-29 23:27:47 -0500

    added roboguice dependency

--- app/src/main/java/prodigious/fabian/com/englishchatapp/MainActivity.java
+++ app/src/main/java/prodigious/fabian/com/englishchatapp/MainActivity.java
@@ -2,16 +2,26 @@ package prodigious.fabian.com.englishchatapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import roboguice.inject.InjectView;
 
 
 public class MainActivity extends Activity {
+    @InjectView(R.id.messages_list)
+    ListView messages;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        populateListView();
     }
 
 
@@ -35,5 +45,15 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void populateListView() {
+        String[] items = {"a","b","c"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.message, items);
+        Log.i("english", "adapter: "+adapter);
+        Toast.makeText(this, "adapter: "+adapter, Toast.LENGTH_LONG);
+        Log.i("english", "messages: "+messages);
+
+        //messages.setAdapter(adapter);
     }
 }
